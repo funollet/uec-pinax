3705d423b21a362d84418ad1962357412bb2fcf2
Jordi Funollet <jordi.f@ati.es>
2008-12-25 21:36:43 +0100

    Initial release.

---
+++ settings.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+# Django settings for basic pinax project.
+
+import sys
+
+from os.path import abspath, dirname, join
+from site import addsitedir
+
+PINAX_ROOT = abspath(join(dirname(__file__), "../pinax-0.5.0"))
+PROJECT_ROOT = abspath(dirname(__file__))
+
+path = addsitedir(join(PINAX_ROOT, "libs/external_libs"), set())
+if path:
+    sys.path = list(path) + sys.path
+sys.path.insert(0, join(PINAX_ROOT, "apps/external_apps"))
+sys.path.insert(0, join(PINAX_ROOT, "apps/local_apps"))
+sys.path.insert(0, join(PROJECT_ROOT, "apps"))
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Jordi Funollet', 'jordi.f@ati.es'),
+)
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'sqlite3'    # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
+DATABASE_NAME = 'uec.db'       # Or path to database file if using sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+# Local time zone for this installation. Choices can be found here:
+# http://www.postgresql.org/docs/8.1/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
+# although not all variations may be possible on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Madrid'
+
+# Language code for this installation. All choices can be found here:
+# http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+# http://blogs.law.harvard.edu/tech/stories/storyReader$15
+LANGUAGE_CODE = 'ca'
+
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = True
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+
+import os.path
+
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), "site_media")
+
+# URL that handles the media served from MEDIA_ROOT.
+# Example: "http://media.lawrence.com"
+MEDIA_URL = '/site_media/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/'
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = 'bk-e2zv3humar79nm=j*bwc=-ymeit(8a20whp3goq4dh71t)s'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django_openidconsumer.middleware.OpenIDMiddleware',
+    'account.middleware.LocaleMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+    'pagination.middleware.PaginationMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+import os.path
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, "templates"),
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+    
+    "notification.context_processors.notification",
+    "announcements.context_processors.site_wide_announcements",
+    "account.context_processors.openid",
+    "account.context_processors.account",
+    "misc.context_processors.contact_email",
+    "misc.context_processors.site_name",
+)
+
+INSTALLED_APPS = (
+    # included
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.humanize',
+    'django.contrib.markup',
+
+    # external
+    'notification', # must be first
+    'emailconfirmation',
+    'mailer',
+    'announcements',
+    'django_openidconsumer',
+    'django_openidauth',
+    'pagination',
+    'timezones',
+    'ajax_validation',
+    'tagging',
+    'bookmarks',
+    
+    # internal (for now)
+    'basic_profiles',
+    'account',
+    'misc',
+    'cms',
+    'links',
+    
+    'django.contrib.admin',
+
+)
+
+ABSOLUTE_URL_OVERRIDES = {
+    "auth.user": lambda o: "/profiles/%s/" % o.username,
+}
+
+AUTH_PROFILE_MODULE = 'basic_profiles.Profile'
+NOTIFICATION_LANGUAGE_MODULE = 'account.Account'
+
+EMAIL_CONFIRMATION_DAYS = 2
+EMAIL_DEBUG = DEBUG
+CONTACT_EMAIL = "feedback@example.com"
+SITE_NAME = "Pinax"
+LOGIN_URL = "/account/login"
+LOGIN_REDIRECT_URLNAME = "what_next"
+
+CMS_LANGUAGE_DEFAULT = 'ca'
+CMS_DEFAULT_TEMPLATE = 'cms/base.html'
+CMS_DEFAULT_CONTENT_TYPE = 'markdown'
+
+# local_settings.py can be used to override environment-specific settings
+# like database and email that differ between development and production.
+try:
+    from local_settings import *
+except ImportError:
+    pass
